c4d60447626c3c30ea15fe38043f79935bd71837
sonddlzb <sonddlzb@gmail.com>
2025-09-24 14:45:41 +0700

    init commit

---
+++ GreeterPod/Source/GreeterPod.swift
@@ -0,0 +1,13 @@
+//
+//  GreeterPod.swift
+//  GreeterPod
+//
+//  Created by Đào Đăng Sơn on 23/9/25.
+//
+
+import Foundation
+
+public struct GreeterPod {
+    public init() {}
+    public func hello(name: String) -> String { "Hello, \(name)" }
+}
