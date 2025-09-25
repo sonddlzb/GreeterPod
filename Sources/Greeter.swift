30d0af175a4d127b1413d210435c58513a17e51b
sonddyopaz <sondd@yopaz.vn>
2025-09-25 21:19:06 +0700

    first commit

---
+++ Sources/Greeter.swift
@@ -0,0 +1,13 @@
+//
+//  Greeter.swift
+//  GreeterKit
+//
+//  Created by đào sơn on 25/9/25.
+//
+
+import Foundation
+
+public struct Greeter {
+    public init() {}
+    public func hello(name: String) -> String { return "Hello \(name)" }
+}
